76c68473e2243562741f7569b2b84234e5117926
Michele Bucca <michele@lenovo>
2016-07-29 17:45:47 +0200

    Added primitive help function to the program

--- busybox/wget-list.sh
+++ busybox/wget-list.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
+#downloads a list of files from a text file containing urls
+
+help() {
+echo "Downloads a list of files from a text file containing urls"
+echo "usage: wget-list.sh [TEXT FILE]"
+exit;}
+
+
 #usage: wget-list.sh [TEXT FILE]
+
+
+if [ -z $1 ]; then help; fi
+
+# fix -why this does not work? Commenting out
+#if [ "$INPUT"=="--help" ]; then help
+#else
+
 cat $1 | while read file; do
 busybox wget $file
 done
+
+#fi
