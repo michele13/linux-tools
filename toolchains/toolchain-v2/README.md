4666bcaff83ec81a775e64172477e62756f868a4
Michele13 <michele.bucca@gmail.com>
2024-01-31 19:03:36 +0100

    added new features and fixed some bugs

--- toolchains/toolchain-v2/README.md
+++ toolchains/toolchain-v2/README.md
@@ -6,7 +6,9 @@ It has been tested various times and installs the cross-toolchain inside the *cr
 
 * It's a simple and self-contained script. No complex syntax is used.
 * Static compiling enabled by default.
-* Compiles under alpine
+* Compiles under alpine.
+* It can resume the build from where it left.
+* You can run an **offline build** 
 
 
 ## Requirements
@@ -28,4 +30,49 @@ Install the following packages in Alpine:
 Install the following packages in Debian:
 
     apt-get install gcc g++ make tar wget xz bison flex texinfo gawk python3
+
+## Offline build
+if you have already downloaded the source tarballs inside the `downloads` directory
+you can run an **offline** build using this command:
+
+    echo OFFLINE=1 >> env
+    ./[script-name].sh
+
+
+or
+
+    OFFLINE=1 ./[script-name].sh
+
+## The `./env` file
+Now when you interrupt a build or if it fails, you can resume it by running the script again.
+this happens thanks to a file called `env`, located alongside the script.
+
+This file, if exists, is loaded by the main script and allows you see what was the latest successful step of the build. 
+It also allows you to ***repeat*** or ***skip*** certain steps.
+an `env` file is automatically created when a step is completed without errors.
+
+The folowing is sample `env` file produced by a complete build:
+
+```
+OFFLINE=1
+EXTRACTED=1
+BINUTILS=1
+KERNEL_HEADERS=1
+GCC_CORE=1
+LIBC_HEADERS=1
+LIBC_CRT=1
+LIBGCC=1
+GLIBC=1
+GCC=1
+
+```
+
+## Archive a toolchain for later use
+
+you can use TAR to archive the toolchain:
+
+    tar cvpJf archive.tar.xz ./cross/$TARGET
     
+and you can extract it later with:
+
+    tar xvpf archive.tar.xz
--- toolchains/toolchain-v2/x86_64-cross-linux-gnu.sh
+++ toolchains/toolchain-v2/x86_64-cross-linux-gnu.sh
@@ -13,27 +13,33 @@ mpfr_ver=4.2.0
 linux_ver=6.4.12
 
 
-cd $(dirname $0) ; CWD=$(pwd); SRC=$CWD/sources; WORK=$CWD/work/$TARGET
+cd $(dirname $0) ; CWD=$(pwd); SRC=$CWD/sources 
+[ -f $CWD/env ] && . $CWD/env
 
-# Linux ARCH
-LARCH=x86_64
+
 TARGET=x86_64-cross-linux-gnu
-INSTALL_DIR=$CWD/cross
+INSTALL_DIR=$CWD/cross/$TARGET
+WORK=$CWD/work/$TARGET
 JOBS=$(nproc)
 
+# CPU ARCH
+#XARCH="armv8-a"
+
+# Linux ARCH
+LARCH=x86
 
 # Extra configure parameters
-# to pass when building GCC. By default is empty
+# to pass when building GCC or GLIBC. By default they are empty
 GCC_CONFIGURE_EXTRA=""
+GLIBC_CONFIGURE_EXTRA=""
 # Configure parameters that will be added to every build
 COMMON_CONFIG=""
 
-# Optimization - static compile
+# Optimization - static
 CC="gcc -static --static"
 CXX="g++ -static --static"
 export CC CXX
 
-# Disable Optimization (uncomment to enable)
 #CFLAGS="-Os -g0"
 #CXXFLAGS="-Os -g0"
 #LDDFLAGS="-s"
@@ -41,8 +47,13 @@ export CC CXX
 
 # Download Tarballs
 
-mkdir -p $CWD/downloads $SRC $WORK
+mkdir -pv $CWD/downloads $SRC $WORK
 cd $CWD/downloads
+
+[ -z $OFFLINE ] && OFFLINE=0
+if [  $OFFLINE == "0" ]; then
+unset OFFLINE
+
 wget -c https://sourceware.org/pub/binutils/releases/binutils-$binutils_ver.tar.xz
 wget -c https://ftp.gnu.org/gnu/gcc/gcc-$gcc_ver/gcc-$gcc_ver.tar.xz
 wget -c https://ftp.gnu.org/gnu/glibc/glibc-$glibc_ver.tar.xz
@@ -50,9 +61,17 @@ wget -c https://ftp.gnu.org/gnu/gmp/gmp-$gmp_ver.tar.xz
 wget -c https://ftp.gnu.org/gnu/mpc/mpc-$mpc_ver.tar.gz
 wget -c https://ftp.gnu.org/gnu/mpfr/mpfr-$mpfr_ver.tar.xz
 wget -c https://www.kernel.org/pub/linux/kernel/v6.x/linux-$linux_ver.tar.xz
-
+echo "OFFLINE=1" >> $CWD/env
+fi
 # Extract tarballs
+[ -z $EXTRACTED ] && EXTRACTED=0
+if [ $EXTRACTED == "0"  ]; then
+unset EXTRACTED
+
 for t in *.tar*; do tar --skip-old-files -xvf $t -C $SRC; done
+echo "EXTRACTED=1" >> $CWD/env
+fi
+
 cd $SRC
 
 # Prepare gcc source dir
@@ -73,18 +92,32 @@ ln -sf . $INSTALL_DIR/$TARGET/usr || true
 
 
 # Build Binutils
+
+echo "=> BINUTILS"
 mkdir -p build-binutils
 cd build-binutils
 
-$SRC/binutils-$binutils_ver/configure --prefix=$INSTALL_DIR --with-sysroot --target=$TARGET --disable-multilib --disable-nls --enable-gprofng=no --disable-werror $COMMON_CONFIG
+[ -z $BINUTILS ] && BINUTILS=0
+if [ $BINUTILS == "0" ]; then
+unset BINUTILS
+
+$SRC/binutils-$binutils_ver/configure --prefix=$INSTALL_DIR --with-sysroot --target=$TARGET --disable-multilib --disable-nls --enable-gprofng=no --enable-default-hash-style=gnu --disable-werror $COMMON_CONFIG
 make -j$JOBS
-make install-strip 
+make install-strip
+echo "BINUTILS=1" >> $CWD/env
+fi 
 
 # Build Linux Kernel Headers
+echo "=> LINUX KERNEL HEADERS"
 
 cd ../
 mkdir -p build-headers
 cd build-headers
+
+[ -z $KERNEL_HEADERS ] && KERNEL_HEADERS=0
+if [ $KERNEL_HEADERS == "0" ]; then
+unset KERNEL_HEADERS
+
 make -C $SRC/linux-$linux_ver mrproper
 
 # make headers_install requires rsync now, so we use a different approach
@@ -92,46 +125,97 @@ make -C $SRC/linux-$linux_ver mrproper
 make -C $SRC/linux-$linux_ver O=$(pwd) ARCH=x86_64 headers
 find usr/include -type f ! -name '*.h' -delete
 cp -rv usr/include $INSTALL_DIR/$TARGET
-
+echo "KERNEL_HEADERS=1" >> $CWD/env
+fi
 
 
 # Build GCC (core only)
+echo "=> GCC CORE"
+
 cd ../
 mkdir -p build-gcc
 cd build-gcc
+
+[ -z $GCC_CORE ] && GCC_CORE=0
+if [ $GCC_CORE == "0" ]; then
+unset GCC_CORE
+
 # Disable libsanitizer is necessary to build GCC without libcrypt installed
 $SRC/gcc-$gcc_ver/configure --prefix=$INSTALL_DIR --target=$TARGET --enable-languages=c,c++ --disable-nls --disable-multilib --disable-libsanitizer $COMMON_CONFIG $GCC_CONFIGURE_EXTRA
 make -j$JOBS all-gcc
 make install-strip-gcc
+echo "GCC_CORE=1" >> $CWD/env
+fi
 
 # Build GLIBC headers and startup files
+echo "=> GLIBC HEADERS"
+
 cd ../
 mkdir -p build-glibc
 cd build-glibc
-$SRC/glibc-$glibc_ver/configure --prefix=$INSTALL_DIR/$TARGET --host=$TARGET --with-headers=$INSTALL_DIR/$TARGET/include --disable-multilib --disable-nls --disable-werror $COMMON_CONFIG
+
+[ -z $LIBC_HEADERS ] && LIBC_HEADERS=0
+if [ $LIBC_HEADERS == "0" ]; then
+unset LIBC_HEADERS
+
+COMMON_CONFIG="" $SRC/glibc-$glibc_ver/configure --prefix=$INSTALL_DIR/$TARGET --host=$TARGET --with-headers=$INSTALL_DIR/$TARGET/include --disable-multilib --disable-nls --disable-werror $GLIBC_CONFIGURE_EXTRA $COMMON_CONFIG
 make -j$JOBS install-bootstrap-headers=yes install-headers
-make -j$JOBS csu/subdir_lib
+echo "LIBC_HEADERS=1" >> $CWD/env
+fi
+
+echo "=> GLIBC STARTUP FILES"
+[ -z $LIBC_CRT ] && LIBC_CRT=0
+if [ $LIBC_CRT == "0" ]; then
+unset LIBC_CRT
+
+make -j$JOBS csu/subdir_lib CC=gcc CXX=g++  
 install csu/crt1.o csu/crti.o csu/crtn.o $INSTALL_DIR/$TARGET/lib
 $TARGET-gcc -nostdlib -nostartfiles -shared  -x c /dev/null -o $INSTALL_DIR/$TARGET/lib/libc.so
 touch $INSTALL_DIR/$TARGET/include/gnu/stubs.h
+echo "LIBC_CRT=1" >> $CWD/env
+fi
 
 # Build libgcc
+echo "=> LIBGCC"
+
 cd ../build-gcc
+
+[ -z $LIBGCC ] && LIBGCC=0
+if [ $LIBGCC == "0" ]; then
+unset LIBGCC
+
 make -j$JOBS all-target-libgcc
 make install-strip-target-libgcc
+echo "LIBGCC=1" >> $CWD/env
+fi
 
 # Finish building glibc
+echo "=> FINISH GLIBC"
+
 cd ../build-glibc
 
-# We use normal values for CC and CXX
-# to avoid failures in the build of the C Library
+[ -z $GLIBC ] && GLIBC=0
+if [ $GLIBC == "0" ]; then
+unset GLIBC
 make -j$JOBS CC=gcc CXX=g++
 make install
+echo "GLIBC=1" >> $CWD/env
+fi
 
 # Finish building GCC
+echo "=> FINISH GCC"
 cd ../build-gcc
+
+[ -z $GCC ] && GCC=0
+if [ $GCC == "0" ]; then
+unset GCC
+
 make -j$JOBS
 make install-strip
+cat $SRC/gcc-$gcc_ver/gcc/limitx.h $SRC/gcc-$gcc_ver/gcc/glimits.h $SRC/gcc-$gcc_ver/gcc/limity.h > \
+  $INSTALL_DIR/lib/gcc/$TARGET/$gcc_ver/include/limits.h
+echo "GCC=1" >> $CWD/env
 
+fi
 echo  -e "\n FINISHED SUCCESSFULLY"
 
